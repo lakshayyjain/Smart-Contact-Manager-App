f6756850e8b4caf226f5b4c1a94f91b2a33de8b0
Lakshay Jain <130989106+lakshayyjain@users.noreply.github.com>
2025-05-06 18:09:03 +0530

    Initial commit of new project

---
+++ src/main/java/com/scm/Controllers/testing.java
@@ -0,0 +1,32 @@
+package com.scm.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class testing {
+
+    @RequestMapping("/")
+    public String getMessage(Model model) {
+        System.out.println("message returned");
+
+        // sending data to view
+        model.addAttribute("name", "Lakshay");
+        model.addAttribute("message", "Welcome to Spring MVC");
+        model.addAttribute("title", "Spring MVC Tutorial");
+
+        return "home";
+    }
+
+    @RequestMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @RequestMapping("/services")
+    public String servicesPage() {
+        return "services";
+    }
+
+}

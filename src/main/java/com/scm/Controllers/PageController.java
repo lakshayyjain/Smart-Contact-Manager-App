771feed5a151d77a40a23e518cd48b84effebcac
Lakshay Jain <130989106+lakshayyjain@users.noreply.github.com>
2025-05-10 00:30:13 +0530

    added the navbar and theme button

--- src/main/java/com/scm/Controllers/PageController.java
+++ src/main/java/com/scm/Controllers/PageController.java
@@ -2,10 +2,11 @@ package com.scm.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class testing {
+public class PageController {
 
     @RequestMapping("/")
     public String getMessage(Model model) {
@@ -29,4 +30,18 @@ public class testing {
         return "services";
     }
 
+    @GetMapping("/contact")
+    public String contactPage() {
+        return "contact";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
 }
